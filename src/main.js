1952c1b35659094c19550033aa43f1ac912c13c2
Leonelzz1 <shickburger88@gmail.com>
2025-09-27 21:27:49 -0300

    debug: Añadir logs para variables de entorno

--- src/main.js
+++ src/main.js
@@ -1,3 +1,7 @@
+console.log("--- DEBUGGING VERCEL ENV VARIABLES ---");
+console.log("Project ID recibido:", import.meta.env.VITE_PROJECT_ID);
+console.log("API Key recibida:", import.meta.env.VITE_API_KEY);
+
 // Importa tu configuración de Firebase aquí. Esto asegura que se inicialice.
 import '@/firebase/config';
 
