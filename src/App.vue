b9cce6ff6da34787980e710084700ee3e7ce1ace
Leonelzz1 <shickburger88@gmail.com>
2025-10-07 13:53:02 -0300

    fix: Hardcode Firebase keys for production deploy

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,24 @@
 <!-- src/App.vue -->
 <script setup>
 import { RouterView } from 'vue-router'
-import AppHeader from '@/components/AppHeader.vue' // 1. Importamos el nuevo header
+import AppHeader from '@/components/AppHeader.vue'
+import SubHeader from '@/components/SubHeader.vue' // 1. Importamos el nuevo subheader
 </script>
 
 <template>
   <AppHeader />
-  <!-- 2. Lo usamos aquí, fuera del RouterView -->
+  <SubHeader />
+  <!-- 2. Lo colocamos justo debajo del header principal -->
 
   <main class="main-content">
     <RouterView />
-    <!-- 3. Aquí se cargarán todas tus páginas (Home, Noticias, etc.) -->
   </main>
 </template>
 
 <style scoped>
 .main-content {
-  /* Añadimos padding para que el contenido no pegue a los bordes */
-  padding: 1rem;
+  padding: 1.5rem 1rem; /* Añadimos un poco más de padding vertical */
 
-  /* Para pantallas más grandes, podemos centrar el contenido */
   max-width: 1200px;
   margin: 0 auto;
 }
