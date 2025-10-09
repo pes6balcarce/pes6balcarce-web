920d2f7c60eb7ef5ee1447146c99362dd7c955fe
Leonelzz1 <shickburger88@gmail.com>
2025-10-08 22:06:20 -0300

    Revert to hardcoded Firebase config

--- src/data/patrocinadores.js
+++ src/data/patrocinadores.js
@@ -4,16 +4,16 @@
 // dentro de la carpeta 'src/assets/patrocinadores/'
 
 // 1. Importa cada imagen que quieras usar
-import adidas from '@/assets/patrocinadores/adidas.png'
+import CostiBarber from '@/assets/patrocinadores/CostiBarber.png'
 // (cuando añadas un nuevo logo, impórtalo aquí)
 
 // 2. Crea y exporta el array de patrocinadores
 export const listaPatrocinadores = [
   {
     id: 1,
-    nombre: 'Adidas',
-    enlace: 'https://www.adidas.com',
-    imagen: adidas, // Usa la variable importada
+    nombre: 'CostiBarber',
+    enlace: 'https://www.instagram.com/costi.barberr',
+    imagen: CostiBarber, // Usa la variable importada
   },
   // Para añadir más, solo copia un bloque de arriba y pégalo aquí
 ]
